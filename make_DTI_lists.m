7896b1619c3df2c25b51d173445fe287638b5b91
John Colby <johncolby@ucla.edu>
2011-03-23 14:18:06 -0700

    Converted make_DTI_lists.m into a function

--- make_DTI_lists.m
+++ make_DTI_lists.m
@@ -1,31 +1,54 @@
-% Make_DTI_lists.m - Setup Pipeline lists for DTI preprocessing workflow
+function make_DTI_lists(exptDir,subIDs,outName,nScans,idStr)
+%MAKE_DTI_LISTS - Setup Pipeline lists for DTI preprocessing workflow
+%For a given experiment directory, this script checks to see how many DTI
+%scan repetitions are present, and generates the appropriate input lists
+%for the DTI_preprocessing.pipe workflow in the <exptDir>/PIPELINE directory.
 %
-% For a given experiment directory, this script checks to see how many DTI
-% scan repetitions are present, and generates the appropriate input lists
-% for the DTI_preprocessing.pipe workflow in the .../exptDir/PIPELINE directory.
+% Syntax: make_DTI_lists(exptDir,subDIs,outName,nScans,idStr)
 %
-% Set Pipeline variable 'exptDir' to the same as below. Then run the workflow.
+% Inputs:
+%    exptDir - Path to base experiment directory. Should contain SUBJECTS and
+%              PIPELINE directories
+%    subIDs  - Vector of subject ID folders in <exptDir>/SUBJECTS (for now, must
+%              be numeric)
+%    outName - Desired output directory name that will be created in each
+%              <exptDir>/SUBJECTS/<subID>/ directory
+%    nScans  - Choose the number of scans to average
+%    idStr   - Specify a string to identify raw DWI series
+%
+% Outputs:
+%
+% Output files:
+%
+% Example:
+%    exptDir = '/path/to/exptDir/';
+%    outName = '2avg';
+%    nScans  = 2;
+%    idStr   = '30DIR';
+%    D       = dir(fullfile(exptDir, 'SUBJECTS/20*'));
+%    subIDs  = str2num(vertcat(D.name));
+%    %subIDs  = 20037;                              % Also can specify subjects(s)
+%    %load(fullfile(exptDir, 'SCRIPTS/subIDs.txt')) % or load up a list
+%    make_DTI_lists(exptDir,subIDs,outName,nScans,idStr)
 %
 % Notes:
-% 1. To run script from command line: matlab -nodisplay < Trio_DTI_lists.m
-% 2. To clear out a whole area, do something like: rm SUBJECTS/*/1avg/dtifit/*
-% 3. Change 'SUBJECTS' to 'TEST' at line 47 to use a test area
+% 1. To clear out a whole area, do something like: rm SUBJECTS/*/1avg/dtifit/*
+% 2. Change 'SUBJECTS' to 'TEST' at line 47 to use a test area
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: DTI_preprocessing.pipe
 
 % Author: John Colby (johncolby@ucla.edu)
+% UCLA Developmental Cognitive Neuroimaging Group (Sowell Lab)
+% Jan 2010
 
-%% Setup
-clear all
-
-exptDir = '/path/to/exptDir/';  % Set experiment directory
-outName = '2avg';               % Set the name of the output folder
-nScans  = 2;                    % Choose the # of scans to average
-idStr   = '30DIR';              % Specify a string to identify raw DWI series
-
-D       = dir(fullfile(exptDir, 'SUBJECTS/20*'));  % Fetch subject list automatically
-subIDs  = str2num(vertcat(D.name));
-% subIDs = 20037;                                  % Also can specify subjects(s)
-% load(fullfile(exptDir, 'SCRIPTS/subIDs.txt'))    % or load up a list
-
+%% Check input arguments
+if nargin < 4 || isempty(nScans) || isempty(outName) || isempty(exptDir)
+    error('Must specify input arguments')
+end
 
 %% Open files for output
 inpt = fopen(fullfile(exptDir, 'PIPELINE', 'inpt.list'),'wt');
